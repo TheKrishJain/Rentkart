9548eed8caf4559af9405b9a9bcaca3e6fdea002
Aradhya <arutripathi011@gmail.com>
2022-05-09 22:28:36 +0530

    Frontend boilerplate

---
+++ client/rentkart/src/App.js
@@ -0,0 +1,13 @@
+import Rentkart from './Component/Rentkart';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Rentkart />
+      </header>
+    </div>
+  );
+}
+
+export default App;
