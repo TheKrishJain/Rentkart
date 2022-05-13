4c49288ac30b4e1ce328d69e36f53c4f9323144c
shreyash <shrevyopari@gmail.com>
2022-05-13 17:49:17 +0530

    shreyash

--- client/rentkart/src/Component/Card.js
+++ client/rentkart/src/Component/Card.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import '../styles/card.scss'
-const Card = (props) => {
+
+const Card = (props) =>
+{
     return (
-        <div className='Card'>
-            <img className='picture' src={props.img} />
-            <p className='CardDescription'>{props.description}</p>
-            <p className='Rent'>Rent- {props.rent}/ -P.M</p>
-        </div>
+<div className='Card'>
+
+    <img  className='picture' src={props.img}/ >
+    <p className='CardDescription'>{props.description}</p>
+    <p className='Rent'>Rent- {props.rent}/ -P.M</p>
+
+</div>
+
+
+
     )
+
+
+
 }
-export default Card;
+export default Card
