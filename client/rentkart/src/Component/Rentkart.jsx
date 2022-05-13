a3c4e3b3de62e8c305fb0a77cef8402685574320
kussshh <kushagragupta3010@gmail.com>
2022-05-13 14:07:34 +0530

    Created Footer

--- client/rentkart/src/Component/Rentkart.jsx
+++ client/rentkart/src/Component/Rentkart.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import '../styles/index.scss';
+import './Footer';
+import Footer from './Footer';
 
 export default function Rentkart() {
   return (
+    <div>
     <h1 className="card">Rentkart</h1>
+    <Footer/>
+    </div>
   )
 }
