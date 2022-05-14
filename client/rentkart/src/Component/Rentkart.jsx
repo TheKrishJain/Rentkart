27846640ad43159c8a421a3272481faf717e3ade
kussshh <kushagragupta3010@gmail.com>
2022-05-14 17:29:57 +0530

    Tried Login / Sidebar Component

--- client/rentkart/src/Component/Rentkart.jsx
+++ client/rentkart/src/Component/Rentkart.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import '../styles/index.scss';
 import './Footer';
 import Footer from './Footer';
+import Login from './Login';
+import Sidebar from './Sidebar';
 
 export default function Rentkart() {
   return (
     <div>
     <h1 className="card">Rentkart</h1>
+    
+
+    <Sidebar/>
     <Footer/>
+
+    
     </div>
   )
 }
