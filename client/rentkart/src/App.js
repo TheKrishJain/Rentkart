31ad13cf501d09be9063ce9c37a5e1bb2494ff50
[Gaurav Pratap Singh] <[gps2001.aljn@gmail.com]>
2022-05-14 23:32:42 +0530

    first commit

--- client/rentkart/src/App.js
+++ client/rentkart/src/App.js
@@ -1,11 +1,18 @@
+<<<<<<< HEAD
 // import './styles/index.scss';
 import Rentkart from './Component/Rentkart';
 
+=======
+import './styles/index.scss';
+import Rentkart from './Component/Rentkart';
+import Card from './Component/Card';
+>>>>>>> b59d916711b1182ba494faa502fc599ba9cc6c55
 function App() {
   const onClickHandelere=()=>{
     
   }
   return (
+<<<<<<< HEAD
     // <div className="App">
     //   <header className="App-header">
     //     <Rentkart />
@@ -13,6 +20,15 @@ function App() {
     // </div>
     <Rentkart >
     </Rentkart>
+=======
+    <div className="App">
+      <header className="App-header">
+        <Rentkart />
+ <Card img='https://www.verywellmind.com/thmb/ciMKi3Kbt6WjkSJZWNNKZv8ufkA=/1715x1286/smart/filters:no_upscale()/low-angle-view-of-building-against-cloudy-sky-750507801-5bc50e0b46e0fb0058c9ef64.jpg' description='abs' rent='free'/>        
+     <Card img ='https://cdn2.psychologytoday.com/assets/styles/manual_crop_1_91_1_1528x800/public/field_blog_entry_teaser_image/2021-05/shutterstock_hauntedhouse.jpg?itok=h-cChspP' description='xyz' rent='free'/>
+      </header>
+    </div>
+>>>>>>> b59d916711b1182ba494faa502fc599ba9cc6c55
   );
 }
 
