63972023e941a7d1ec1bf66dc33598f105511e2c
Aradhya <arutripathi011@gmail.com>
2022-05-16 21:51:23 +0530

    Merge branch 'backend_ashish' into rentkart/room-crud-apis

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,11 +1,14 @@
 import Mongoose from 'mongoose';
 import validator from 'validator';
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 const { Schema, model } = Mongoose;
 
 const userSchema = new Schema({
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -25,7 +28,17 @@ const userSchema = new Schema({
       }
     }
   },
-  name: { type: String, required: true, trim: true },
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  tokens: [{
+    token: {
+      type: String,
+      required: true
+    }
+  }],
   address: { type: String },
   marital_status: { type: String },
   phone_number: { type: String },
@@ -33,6 +46,41 @@ const userSchema = new Schema({
   posted_room_ids: [ { type: String } ],
   room_applicant_ids: [ { } ],
   interested_room_ids: [ { type: String } ],
+})
+
+userSchema.methods.generateAuthToken = async function() {
+  const user = this
+  const token = jwt.sign({ _id:user._id.toString() }, 'mynameisashishsingh')
+
+  user.tokens = user.tokens.concat({ token })
+  await user.save()
+
+  return token
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({email})
+  
+  if(!user) {
+    throw new Error ('Unable to login')
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password)
+
+  if(!isMatch) {
+    throw new Error('Unable to login')
+  }
+
+  return user
+}
+
+userSchema.pre('save', async function (next) {
+  const user = this 
+  if(user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password,8)
+  }
+
+  next()
 })
 
 const User = new model('user', userSchema);
