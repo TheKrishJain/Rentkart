0fe6cb7699db2eadc89a8ee7c333c4b5820880c5
Aradhya <arutripathi011@gmail.com>
2022-05-16 19:00:27 +0530

    Landing page along with few other pages done

--- client/rentkart/src/pages/LandingPage.jsx
+++ client/rentkart/src/pages/LandingPage.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
+import Banner from "../images/rentKartBanner.webp"
+import { BenefitsData, CardList } from '../static/Cards';
+import Card from '../component/Card';
+import "../styles/_landingPage.scss";
 
 export default function LandingPage() {
   return (
-    <div>
-      
+    <div className='landing-container'>
+      <img src={Banner} alt="banner" className='landing-container__banner'/>
+      <h1><span className='title'>RentKart</span>- Just Rent it!!</h1>
+      <h2>Available Rooms</h2>
+      <div className='landing-container__room-cards'>
+        {CardList.map((card) =>
+        <Link to={`room/:${card.id}`} > 
+          <Card roomPhoto={card.img} description={card.description} key={card.id} rentalPrice={card.rentalPrice} />
+        </Link>
+        )}
+      </div>
+      <Link to="room-list" >
+        <span className='landing-container__explore-btn'>View All Rooms</span>
+      </Link>
+      <span className='benefit-heading'>Benefits of Listing with us</span>
+      <div className='flex-row benefit_container'>
+        {BenefitsData.map((benefit, index) => 
+          <div className='benefit_box' key={index} >
+            <img className='benefit_box__img' src={benefit.img} alt={benefit.title} />
+            <div className='benefit_box__text'>
+              <span className='benefit-title'>{benefit.title}</span>
+              <p className='benefit-description'>{benefit.description}</p>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
--- client/rentkart/src/pages/Layout.jsx
+++ client/rentkart/src/pages/Layout.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { shallowEqual, useSelector } from 'react-redux';
 import Footer from '../component/Footer'
 import Rentkart from '../component/Rentkart'
+import SideBar from '../component/Sidebar'
+import Login from "../component/Login";
 
 export default function Layout({children}) {
+  const {isLoginModalOpen} = useSelector((state) => ({
+    isLoginModalOpen: state.userStore.isLoginModalOpen,
+  }), shallowEqual);
   return (
     <div>
       <Rentkart />
+      <SideBar isOpen={isLoginModalOpen}>
+        <Login />
+       </SideBar>  
       <div className='main-container'>
         {children}
       </div>
